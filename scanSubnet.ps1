86d46afe79383b8d6f776847013857171c410584
Jacob Kelley <exaybachay.ak@gmail.com>
2022-07-19 21:13:16 -0800

    Add logging
    Added tee-object for all output to generate log file.

--- scanSubnet.ps1
+++ scanSubnet.ps1
@@ -2,11 +2,21 @@ param(
   [Parameter(Mandatory=$False)]$ports,
   [Parameter(Mandatory=$False)][Boolean]$sequential,
   [Parameter(Mandatory=$False)][Boolean]$random,
-  [Parameter(Mandatory=$False)][Boolean]$resolveDns
+  [Parameter(Mandatory=$False)][Boolean]$resolveDns,
+  [Parameter(Mandatory=$False)][Boolean]$quick
 )
 
+# Configure logging
+$logname = "$((pwd).path)\powerScan_$(get-date -format "yyyyMMMd")_$(get-date -format "hhmm").log"
+
 # Set parameters if not provided
-if(!($ports)){ $ports = @(22,23,80,135,443,8080) }
+if(!($ports)){ 
+  $ports = @(22,23,80,135,443,8080) 
+  write-output "--->>>   Ports not specified, defaulting to scan ports 22, 23, 80, 135, 443, and 8080---" | tee-object -filepath $logname -append
+}
+if(!($quick)){ write-output "--->>>   Quick parameter not set, defaulting to slower scan" | tee-object -filepath $logname -append} 
+if(!($sequential)){ write-output "--->>>   Sequential parameter not set, defaulting to pseudo random scan" | tee-object -filepath $logname -append }
+write-output " " | tee-object -filepath $logname -append
 
 function fastping{
   [CmdletBinding()]
@@ -28,13 +38,13 @@ function fastping{
   }
 }
 
-function testPort ($comp, $port) {
+function testPort ($comp, $port, $delay) {
   # Improving speed of timeouts
   #   https://superuser.com/questions/805621/test-network-ports-faster-with-powershell
   $requestCallback = $state = $null
   $tcpClient = New-Object System.Net.Sockets.TCPClient
   $beginConnect = $tcpClient.BeginConnect($comp,$port,$requestCallback,$state)
-  Start-sleep -milli 10
+  Start-sleep -milli $delay
   if($tcpClient.Connected) { $open = $True } else { $open = $False }
   $tcpClient.Close()
   [pscustomobject]@{computername=$comp;port=$port;open=$open}
@@ -51,9 +61,9 @@ if ($activeIP.ipsubnet -eq "255.255.255.0"){
   $classCIpAddr = ($activeIP.ipAddress | sls -Pattern $classCPattern).Matches.Value
 
   $usermessage = $classCIpAddr + "0/24"
-  write-output "Scanning entire $usermessage subnet..."
-  write-output "========================================="
-  write-output " "
+  write-output "Scanning entire $usermessage subnet..." | tee-object -filepath $logname -append
+  write-output "=========================================" | tee-object -filepath $logname -append
+  write-output " " | tee-object -filepath $logname -append
 
   $scanrange = @(1..255)
   foreach ($ipaddr in $scanrange){
@@ -79,20 +89,24 @@ if ($activeIP.ipsubnet -eq "255.255.255.0"){
       }
     }
     if ($pingStatus -eq $True){
-      write-output "Scanning $scanIp..."
+      write-output "Scanning $scanIp..." | tee-object -filepath $logname -append
       if($resolveDns){
-        $hn = Resolve-DnsName $scanIp
-        $hn = $hn.namehost        
+        $hn = Resolve-DnsName $scanIp | tee-object -filepath $logname -append
+        $hn = $hn.namehost | tee-object -filepath $logname -append
       }
       $notlistening = 0
       foreach($port in $ports){
-        $test = testPort $scanIp $port
+        if($quick){
+          $test = testPort $scanIp $port 10
+        } else {
+          $test = testPort $scanIp $port 30
+        }
         if($test.open){
-          write-output "$scanIp is listening on $port!"
+          write-output "$scanIp is listening on $port!" | tee-object -filepath $logname -append
         } else {
           $notlistening += 1
           if($notlistening -eq $ports.count ){
-            write-output "$scanIp is not listening on any ports"
+            write-output "$scanIp is not listening on any ports" | tee-object -filepath $logname -append
           }
         }
       }
